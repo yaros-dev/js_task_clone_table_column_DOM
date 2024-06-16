a65e8e525dc5c617817f72d2e0d9c79269dbdda0
Yaroslav Pazynenko <yaroslavpazynenko@gmail.com>
2024-06-16 09:10:19 +0300

    make build

---
+++ index.3f8f707b.js
@@ -0,0 +1,2 @@
+function e(e){e.querySelectorAll("tr").forEach(function(e){return function(e,t){var o=e.cells[1].cloneNode(!0);e.insertBefore(o,e.lastElementChild)}(e,0)})}var t=document.querySelector("tbody"),o=document.querySelector("thead"),r=document.querySelector("tfoot");e(o),e(t),e(r);
+//# sourceMappingURL=index.3f8f707b.js.map
